c4ce337adb4557c7a1d90be41d6dca7f37bc9f5b
Ralphael Obadiaru <ralph.obadiaru@gmail.com>
2024-02-06 16:46:31 +0000

    New computer object created

---
+++ src/main/java/Computer.java
@@ -0,0 +1,10 @@
+public class Computer {
+
+    private int storage;
+    private String printerModel;
+
+    public Computer(int inputStorage, String inputPrinterModel) {
+        this.storage = inputStorage;
+        this.printerModel = inputPrinterModel;
+    }
+}
